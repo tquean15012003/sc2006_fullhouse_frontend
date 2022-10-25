37e904bcc7f2351254c951658a3253d83274ce19
tquean15012003 <tquean15012003@gmail.com>
2022-10-25 22:57:16 +0800

    Change to deploy again

--- src/pages/SubmitProfilePage/SubmitProfilePage.js
+++ src/pages/SubmitProfilePage/SubmitProfilePage.js
@@ -283,7 +283,7 @@ export default function SubmitProfilePage() {
                       type='number'
                       id='investments'
                       className='text-base md:text-xl bg-gray-50 border border-gray-300 text-gray-900 rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-                      placeholder='5%'
+                      placeholder='5'
                       required
                       onChange={formik.handleChange}
                       value={formik.values.investments}
--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,4 +1,4 @@
-export const DOMAIN = "http://localhost:4000/api/v1";
+export const DOMAIN = "http://ec2-18-136-100-233.ap-southeast-1.compute.amazonaws.com:4000/api/v1";
 
 export const TOKEN = "token";
 
